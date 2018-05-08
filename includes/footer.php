866dcd96affca41c3224f0fb21ccbbc838c01285
gijsomans <gijsomans@gmail.com>
2018-05-08 11:26:43 +0200

    website

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+    </main>
+    <footer>
+    <strong>EENMAAL<br>ANDERMAAL</strong>
+
+    </footer>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Eenmaal Andermaal</title>
+        <link rel="icon" href="images/favicon.ico">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- UIKit core functions -->
+        <link rel="stylesheet" href="../css/uikit.css" />
+        <link rel="stylesheet" href="../css/uikit-rtl.css" />
+        <script src="../js/uikit.min.js"></script>
+        <script src="../js/uikit-icons.min.js"></script>
+    </head>
+    <main>
